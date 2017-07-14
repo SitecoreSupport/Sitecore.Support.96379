e71336d0ab477d7dbf0088142633293a4876b99c
DK\AMCH <amch@sitecore.net>
2017-07-14 06:48:44 +0300

    commit patch

---
+++ src/Sitecore.Support.96379/Speak/Applications/InsertLinkDialogTreeFix.cs
@@ -0,0 +1,36 @@
+﻿using Sitecore.Speak.Applications;
+using Sitecore.Web;
+using System;
+using System.Xml.Linq;
+
+namespace Sitecore.Support.Speak.Applications
+{
+  public class InsertLinkDialogTreeFix : InsertLinkDialogTree
+  {
+    private static string GetXmlAttributeValue(XElement element, string attrName)
+    {
+      if (element.Attribute(attrName) == null)
+      {
+        return string.Empty;
+      }
+      return element.Attribute(attrName).Value;
+    }
+
+    public override void Initialize()
+    {
+      base.Initialize();
+      if (WebUtil.GetQueryString("hdl") != string.Empty)
+      {
+        string str = UrlHandle.Get()["va"];
+        if (!string.IsNullOrEmpty(str))
+        {
+          XElement element = XElement.Parse(str);
+          if (GetXmlAttributeValue(element, "linktype") == "internal")
+          {
+            base.Target.Parameters["selectedItem"] = GetXmlAttributeValue(element, "target");
+          }
+        }
+      }
+    }
+  }
+}
---
+++ src/Sitecore.Support.96379/sitecore/shell/client/Sitecore/Speak/InsertLinkDialogTreeFixPageCode.js
@@ -0,0 +1,18 @@
+﻿define(["sitecore"], function (Sitecore) {
+    var InsertLinkPC = Sitecore.Definitions.App.extend({
+        initialized: function () {
+            var app = this;
+            app.Target.set("CheckedItems", app.Target.viewModel.$el.attr("data-sc-selecteditem"));
+            app.TargetsDataSource.on("change:hasItems", function () {
+                var a = app.TargetsDataSource["attributes"].items;
+                a.forEach(function (item, i, a) {
+                    if (item.Text == app.Target["attributes"].CheckedItems) {
+                        app.Target.set("selectedItem", item);
+                    }
+                });
+            })
+        }
+    });
+
+    return InsertLinkPC;
+});
